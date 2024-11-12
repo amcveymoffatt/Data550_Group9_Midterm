1057f6274b0afe17818712a79f5fd639ab32e6cf
MOI13 <92569065+MOI13@users.noreply.github.com>
2024-11-12 10:39:36 -0500

    Initial Commit from Shirley, change the code file structure, data file structure, Makefile and Report.Rmd

---
+++ Code/01_load_data.R
@@ -0,0 +1,12 @@
+here::i_am(
+  "Code/01_load_data.R"
+)
+
+
+f75 <- read.csv(here::here("Data/f75_interim.csv"))
+
+f75$arm <- factor(f75$arm)
+f75$arm <- relevel(f75$arm, ref = "Standard F75")
+
+
+saveRDS(f75, here::here("Data/intermediate/f75.rds"))
