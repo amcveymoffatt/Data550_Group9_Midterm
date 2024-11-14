c670d0d9afaf290bd7f9c6436871e4578678b06e
danyangc <98071715+danyangc@users.noreply.github.com>
2024-11-14 16:33:39 -0500

    added code to create bar plot

--- Code/04_weight_diff.R
+++ Code/04_weight_diff.R
@@ -1,4 +1,5 @@
 library(here)
+library(ggplot2)
 
 here::i_am(
   "Code/04_weight_diff.R"
@@ -7,4 +8,18 @@ here::i_am(
 data <- read.csv(here::here("f75_interim.csv"))
 
 wilcox.test(weight ~ arm, data = data)
+
+bar <- ggplot(data, aes(x = arm, y = weight, fill = arm)) +
+  geom_boxplot() +
+  labs(title = "Weight Comparison between Standard and Modified F75 Groups",
+       x = "Group (Standard F75 vs. Modified F75)",
+       y = "Weight") +
+  theme_minimal()
+
+ggsave(
+  here::here("Output/bar.png"),
+  plot = bar,
+  device = "png"
+)
+
 
